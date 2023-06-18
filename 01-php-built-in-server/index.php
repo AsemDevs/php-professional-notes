a38edcf594130cdf1026d809714c866780897efe
Asem Hamdi <asemhamdi97@gmail.com>
2023-06-18 06:49:24 +0300

    Add index.php with links to examples

---
+++ 01-php-built-in-server/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Built-in Server Examples</title>
+</head>
+<body>
+    <h1>PHP Built-in Server Examples</h1>
+    <ul>
+        <li><a href="examples/hello.php">Hello World Example</a></li>
+    </ul>
+</body>
+</html>
